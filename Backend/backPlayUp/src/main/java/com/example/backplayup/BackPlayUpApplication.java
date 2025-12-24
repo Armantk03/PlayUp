b4e45180d7caa58e1e5149c596013b796c883007
arman <arman200328@gmail.com>
2025-12-24 12:45:38 +0100

    creating backend project structure

---
+++ Backend/backPlayUp/src/main/java/com/example/backplayup/BackPlayUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.backplayup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPlayUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackPlayUpApplication.class, args);
+    }
+
+}
---
+++ Backend/backPlayUp/src/test/java/com/example/backplayup/BackPlayUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backplayup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPlayUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
